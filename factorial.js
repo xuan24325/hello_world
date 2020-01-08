f92cb2e18518b73f558a5626b964479df6aac4b0
Xuan l <xuan24325@gmail.com>
2020-01-08 10:59:33 -0800

    num *=i

--- factorial.js
+++ factorial.js
@@ -1,1 +1,11 @@
 
+function factorialize(num){
+  if (num ===0 || num==1){
+    return 1;
+  }
+  for (var i=num - 1; i>=1; i--){
+    num=num*i;
+  }
+  return num;
+}
+console.log(factorialize(4))
